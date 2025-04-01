4aeb2235542ceb06c68b94d959fbe052b8188d28
AT1927 <atorresmunoz88@gmail.com>
2025-03-31 19:21:50 -0500

    Inicializando proyecto users - appwrite

---
+++ lib/core/constants/appwrite_constants.dart
@@ -0,0 +1,7 @@
+// 3.29
+class AppwriteConstants {
+  static const String endpoint = 'https://cloud.appwrite.io/v1';
+  static const String projectId = '67eb25c000290db7a7de';
+  static const String databaseId = '67eb26980003a17802ba';
+  static const String collectionId = '67eb26b6002422b47e95';
+}
